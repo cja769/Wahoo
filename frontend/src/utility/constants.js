a2f4cb04bde4257bbecfc5664caeb22cdb2f2e36
Jay Aisenbrey <jaisenbrey@apple.com>
2023-05-22 20:42:39 -0500

    Remove port that was added for local testing

--- frontend/src/utility/constants.js
+++ frontend/src/utility/constants.js
@@ -3,5 +3,5 @@ export default function getBackendUrl() {
     if (process.env.NODE_ENV === 'development') {
         return "http://wahoo.jay.com:8082"
     }
-    return "http://wahoo.the-boys-pickem.com:8082"
+    return "http://wahoo.the-boys-pickem.com"
 };
